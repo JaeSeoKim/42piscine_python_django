61bf9878046a26e7e2f01eed543efee18c17553c
jaeskim <jaeskim@student.42seoul.kr>
2021-05-28 17:26:21 +0900

    :sparkles: finish D04/ex00

--- d04/ex00/D04/D04/settings.py
+++ d04/ex00/D04/D04/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ex00.apps.Ex00Config'
 ]
 
 MIDDLEWARE = [
--- d04/ex00/D04/ex00/views.py
+++ d04/ex00/D04/ex00/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
-    template = loader.get_template('ex00/index.html')
-    context = {}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'ex00/index.html')
