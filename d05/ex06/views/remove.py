32911d8fa73941b8766b83d8acd21cd9b4888f1c
jaeskim <jaeskim@student.42seoul.kr>
2021-06-03 16:20:00 +0900

    :fire: remove migrations code

--- d05/ex06/views/remove.py
+++ d05/ex06/views/remove.py
@@ -6,10 +6,8 @@ from ..forms import RemoveForm
 import psycopg2
 
 
-TABLE_NAME = "ex04_movies"
-
-
 class Remove(View):
+    TABLE_NAME = "ex06_movies"
     template = "ex06/remove.html"
     conn = psycopg2.connect(
         dbname=settings.DATABASES['default']['NAME'],
@@ -21,7 +19,7 @@ class Remove(View):
 
     def get(self, request):
         SELECT_TABEL = f"""
-            SELECT * FROM {TABLE_NAME};
+            SELECT * FROM {self.TABLE_NAME};
             """
         try:
             with self.conn.cursor() as curs:
@@ -36,7 +34,7 @@ class Remove(View):
 
     def post(self, request):
         SELECT_TABEL = f"""
-            SELECT title FROM {TABLE_NAME};
+            SELECT title FROM {self.TABLE_NAME};
             """
         try:
             with self.conn.cursor() as curs:
@@ -48,7 +46,7 @@ class Remove(View):
             print(e)
         data = RemoveForm(choices, request.POST)
         DELETE_SQL = f"""
-            DELETE FROM {TABLE_NAME} WHERE title = %s
+            DELETE FROM {self.TABLE_NAME} WHERE title = %s
             """
         if data.is_valid() == True:
             try:
